5eb27815573f3e55204a466e3f06935b668bb345
Alina Salinas Mendoza <alinasalmen995@gmail.com>
2018-10-03 11:42:41 -0500

    Agregando botones de los municipios en el view de estado

--- App/Views/Estado.php
+++ App/Views/Estado.php
@@ -1,5 +1,27 @@
-<div class="container-fluid">
+<div class="container-fluid p-5">
+<!-- Presentacion del estado de Guerrero  -->
     <div class="row">
-        
+        <h2>El estado de Guerrero</h2> 
+        <br><br><br>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam quas rerum excepturi repudiandae voluptatum libero inventore nihil modi? Dolorem voluptatum dignissimos aut ipsa! Veniam omnis architecto nostrum voluptates aspernatur?
+        </p>       
+        <br>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga magnam ex minima dolorem deleniti eligendi officiis minus et laboriosam, tenetur distinctio corporis atque vitae nesciunt doloremque corrupti aperiam nemo?
+        </p>
+        <br>
     </div>
+
+    <!-- Seccion para escoger las regiones con unos simples botones-->
+    <div class="row">
+        <button class="btn btn-warning btn-lugares m-3" id="MuniAca">Acapulco</button>
+        <button class="btn btn-warning btn-lugares m-3" id="MuniCC">Costa Chica</button>
+        <button class="btn btn-warning btn-lugares m-3" id="MuniCG">Costa Grande</button>
+        <button class="btn btn-warning btn-lugares m-3" id="MuniMon">La Montaña</button>
+        <button class="btn btn-warning btn-lugares m-3" id="MuniTC">Tierra Caliente</button>
+        <button class="btn btn-warning btn-lugares m-3" id="MuniNor">Norte</button>
+        <button class="btn btn-warning btn-lugares m-3" id="MuniCen">Centro</button>
+    </div>
+    
 </div>
