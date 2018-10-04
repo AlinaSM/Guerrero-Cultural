8a6f022454bbeab2562a4e7fa0cf5896577c7988
AlinaSM <alinasalmen995@gmail.com>
2018-10-04 11:21:48 -0500

    Quitando comentarios innecesarios, y modificando el view de Estados

--- App/Views/Estado.php
+++ App/Views/Estado.php
@@ -1,29 +1,53 @@
 <div class="container-fluid p-5">
-<!-- Presentacion del estado de Guerrero  -->
+    <!-- Presentacion del estado de Guerrero  -->
     <div class="row">
-        <h2>El estado de Guerrero</h2> 
+        <h2>El estado de Guerrero</h2>
         <br><br><br>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam quas rerum excepturi repudiandae voluptatum libero inventore nihil modi? Dolorem voluptatum dignissimos aut ipsa! Veniam omnis architecto nostrum voluptates aspernatur?
-        </p>       
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam quas rerum excepturi repudiandae
+            voluptatum libero inventore nihil modi? Dolorem voluptatum dignissimos aut ipsa! Veniam omnis architecto
+            nostrum voluptates aspernatur?
+        </p>
         <br>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga magnam ex minima dolorem deleniti eligendi officiis minus et laboriosam, tenetur distinctio corporis atque vitae nesciunt doloremque corrupti aperiam nemo?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga magnam ex minima dolorem deleniti
+            eligendi officiis minus et laboriosam, tenetur distinctio corporis atque vitae nesciunt doloremque corrupti
+            aperiam nemo?
         </p>
         <br>
     </div>
 
     <!-- Seccion para escoger las regiones con unos simples botones-->
-    <div class="row">
-        <button class="btn btn-warning btn-lugares m-3" id="MuniAca">Acapulco</button>
-        <button class="btn btn-warning btn-lugares m-3" id="MuniCC">Costa Chica</button>
-        <button class="btn btn-warning btn-lugares m-3" id="MuniCG">Costa Grande</button>
-        <button class="btn btn-warning btn-lugares m-3" id="MuniMon">La Montaña</button>
-        <button class="btn btn-warning btn-lugares m-3" id="MuniTC">Tierra Caliente</button>
-        <button class="btn btn-warning btn-lugares m-3" id="MuniNor">Norte</button>
-        <button class="btn btn-warning btn-lugares m-3" id="MuniCen">Centro</button>
+
+    <div class="row btn-group btn-group-justified">
+
+        <button class="btn btn-warning btn-lugares m-2" id="MuniAca">Acapulco</button>
+        <button class="btn btn-warning btn-lugares m-2" id="MuniCC">Costa Chica</button>
+        <button class="btn btn-warning btn-lugares m-2" id="MuniCG">Costa Grande</button>
+        <button class="btn btn-warning btn-lugares m-2" id="MuniMon">La Montaña</button>
+
+        <button class="btn btn-warning btn-lugares m-2" id="MuniTC">Tierra Caliente</button>
+        <button class="btn btn-warning btn-lugares m-2" id="MuniNor">Norte</button>
+        <button class="btn btn-warning btn-lugares m-2" id="MuniCen">Centro</button>
     </div>
 
-    
-    
+    <!--  -->
+
+    <main class=" row contenido-principal">
+        <div class="row">
+            <div class="col-lg-12 m-4">
+                <h2 id="nombreRegionMostrar">Region: Acapulco</h2>
+            </div>
+        </div>
+        <div class="row p-2">
+            <div class="col-lg-7 bg-yl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo officia molestias deleniti ipsam quibusdam officiis suscipit ducimus commodi. Vel beatae nisi repudiandae molestias itaque officia saepe doloribus cum, vero quod?
+            </div>
+            <div class="col-lg-5 bg-yl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quis, assumenda consequuntur pariatur ratione quam perspiciatis. Dignissimos sed dolore, ad officiis natus tempore nobis dolores illum voluptatibus, blanditiis libero nulla.
+            </div> 
+        </div>
+
+    </main>
+
 </div>
--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 require_once('App/Controllers/Autoload.php');
 $autoload = new Autoload();
 
-
-
 if(isset($_GET['r'])){
     $route =  isset($_GET['r']) ? $_GET['r'] : 'Home' ;
     $gro_cultural = new Router( $route );
@@ -11,10 +9,5 @@ if(isset($_GET['r'])){
     $route_admin =  isset($_GET['a']) ? $_GET['a'] : 'Login' ;
     $gro_cultural_admin = new Router( $route_admin );
 }
-
-
-//require_once('App/Views/Head.php');
-//require_once('App/Views/Header.php');
-//require_once('App/Views/Footer.php');
 
 ?>
